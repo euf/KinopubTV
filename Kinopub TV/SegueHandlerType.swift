e5531f767adcbac03b40a2806ba408375bee6014
Peter Tikhomirov <peter.tikhomirov@gmail.com>
2016-11-23 09:39:50 +0100

    Segue handling protocol
    thanks Natasha! :)

---
+++ Kinopub TV/SegueHandlerType.swift
@@ -0,0 +1,61 @@
+//
+//  SegueHandlerType.swift
+//  Kinopub TV
+//
+//  Created by Peter on 20.11.16.
+//  Copyright © 2016 Peter Tikhomirov. All rights reserved.
+//
+
+import UIKit
+
+protocol SegueHandlerType {
+	associatedtype SegueIdentifier: RawRepresentable
+}
+
+extension SegueHandlerType where Self: UIViewController, SegueIdentifier.RawValue == String {
+	
+	func performSegueWithIdentifier(segueIdentifier: SegueIdentifier, sender: AnyObject?) {
+		performSegue(withIdentifier: segueIdentifier.rawValue, sender: sender)
+	}
+	
+	func segueIdentifierForSegue(segue: UIStoryboardSegue) -> SegueIdentifier {
+		guard let identifier = segue.identifier, let segueIdentifier = SegueIdentifier(rawValue: identifier) else {
+			fatalError("Invalid segue identifier \(segue.identifier).")
+		}
+		return segueIdentifier
+	}
+}
+
+// Usage:
+
+/*
+
+enum SegueIdentifier: String {
+	case TheRedPillExperience
+	case TheBluePillExperience
+}
+
+override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+
+// 🎉 goodbye pyramid of doom!
+switch segueIdentifierForSegue(segue) {
+case .TheRedPillExperience:
+print("😈")
+case .TheBluePillExperience:
+print("👼")
+}
+}
+
+@IBAction func onRedPillButtonTap(sender: AnyObject) {
+// ✅ this is how I want to write my code! Beautiful!
+performSegueWithIdentifier(.TheRedPillExperience, sender: self)
+}
+
+@IBAction func onBluePillButtonTap(sender: AnyObject) {
+performSegueWithIdentifier(.TheBluePillExperience, sender: self)
+}
+
+
+
+
+*/
